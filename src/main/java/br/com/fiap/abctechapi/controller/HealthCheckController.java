fc0bffcfd568ac1182152d3ac6ddbb5fc2959bc1
Daniel Akio Teixeira <danielakioteixeira2@gmail.com>
2023-03-30 22:55:49 -0300

    Version Component

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,17 +1,21 @@
 package br.com.fiap.abctechapi.controller;
 
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
+
+    private final VersionComponent versionComponent;
+
+    public HealthCheckController(@Autowired VersionComponent versionComponent) {
+        this.versionComponent = versionComponent;
+    }
 
     @GetMapping
     public ResponseEntity<String> status(){
@@ -19,12 +23,7 @@ public class HealthCheckController {
     }
 
     @GetMapping("version")
-    public ResponseEntity<String> version() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-
-        properties.load(inputStream);
-
-        return ResponseEntity.ok(properties.get("build_name") + "-" + properties.get("build_version"));
+    public ResponseEntity<String> version() {
+        return ResponseEntity.ok(this.versionComponent.getVersion());
     }
 }

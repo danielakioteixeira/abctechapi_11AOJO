a51b929f79d9f6fe3166e1c17764b4dfb0e8f24c
Daniel Akio Teixeira <danielakioteixeira2@gmail.com>
2023-04-10 22:10:38 -0300

    Adiciona Mysql, JPA e testes

---
+++ src/test/java/br/com/fiap/abctechapi/service/AssistanceServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AssistanceServiceTest {
+
+    @Test
+    public void test_lista_assistance() {
+
+    }
+}

f67d302b2b74c736d7933f0d3278e1ab78e4fd33
Daniel Akio Teixeira <danielakioteixeira2@gmail.com>
2023-05-24 20:24:48 -0300

    Corrige erros de nomenclatura

--- src/main/java/br/com/fiap/abctechapi/application/dto/AssistResponseDto.java
+++ src/main/java/br/com/fiap/abctechapi/application/dto/AssistResponseDto.java
@@ -7,6 +7,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class AssistResponseDto {
     private Long id;
-    private String title;
+    private String name;
     private String description;
 }
--- src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/assists")
+@RequestMapping("/assistance")
 @RestController
 public class AssistanceController {
     private final AssistanceApplicationImpl assistanceApplicationImpl;
